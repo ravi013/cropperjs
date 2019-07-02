5ddb8e629bd30864309f747fd98517d87615b2e9
ravi <ravi013@gmail.com>
2019-07-02 15:25:32 +0530

    fix for mobile

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,7 +1,7 @@
 export const IS_BROWSER = typeof window !== 'undefined';
 export const WINDOW = IS_BROWSER ? window : {};
 export const IS_TOUCH_DEVICE = IS_BROWSER ? 'ontouchstart' in WINDOW.document.documentElement : false;
-export const HAS_POINTER_EVENT = IS_BROWSER ? 'PointerEvent' in WINDOW : false;
+export const HAS_POINTER_EVENT = IS_TOUCH_DEVICE ?false :(IS_BROWSER ? 'PointerEvent' in WINDOW : false);
 export const NAMESPACE = 'cropper';
 
 // Actions
